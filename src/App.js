22588f413fade1079ee987237a64131cc4b79eaa
Foxglovery <foxglovery@gmail.com>
2024-04-22 15:01:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState(["oranges", "apples", "candy"]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="list-container">
+      <ListDisplay items={items} />
+      <InputText
+        handleSubmit={(item) => {
+          setItems(item.concat(item));
+        }}
+      />
     </div>
   );
 }
 
+const ListItem = (props) => <li>{props.name}</li>;
+
+const ListDisplay = (props) => {
+  const items = props.items.map((item, i) => <ListItem key={i} name={item} />);
+  return <ul>{items}</ul>;
+};
+
+const InputText = (props) => {
+  const [value, setValue] = useState("");
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        props.handleSubmit(value);
+        setValue("");
+      }}
+    >
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+};
 export default App;
